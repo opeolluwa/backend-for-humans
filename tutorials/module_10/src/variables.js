43eed1a10d4e622eb3b1190226448ee93b1ff3e6
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-03-18 01:13:52 +0100

    update

---
+++ tutorials/module_10/src/variables.js
@@ -0,0 +1,18 @@
+// VARIABLES AND VALUES
+
+/**
+ * using variables and values in JavaScript
+ * the following keywords are used :
+ * let, var and const
+ */
+
+let accountBalance = 10000;
+
+// update the account balance.
+accountBalance = 450000;
+
+
+const gender = "male"
+
+
+var friend  = "Alex"
